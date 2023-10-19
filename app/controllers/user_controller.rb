448450d93ce98e6013ddd2fc20e79a00b2b42655
SoliriusAdmin196 <soliriusadmin196@Soliriun196sMBP.broadband>
2023-10-19 10:27:07 +0100

    adding coder in user_controller to display resources

--- app/controllers/user_controller.rb
+++ app/controllers/user_controller.rb
@@ -1,2 +1,25 @@
 class UserController < ApplicationController
+    def index
+        @entries = Entry.all
+    end
+
+    def new 
+        @entry = Entry.new()
+    end
+
+    def create 
+        @entry = Entry.new(entry_url)
+        if @entry.save
+            redurect_to root_url
+        else
+            render :new
+        end
+    end
+
+    private
+
+    def entry_params
+        params.require(:entry).permit(:name)
+    end
+
 end
